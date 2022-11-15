c10be2ce475b9a810a628f8817f0ff83c881a471
Ground <27danny@naver.com>
2022-11-15 16:39:25 +0900

    Create StoryView of SF version

--- IKU/IKU/App/SceneDelegate.swift
+++ IKU/IKU/App/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
@@ -14,11 +15,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
-        let firstViewController = UINavigationController()
+//        let firstViewController = UINavigationController()
+        let firstViewController = UIHostingController(rootView: SF_StoryView())
         window?.rootViewController = firstViewController
         window?.makeKeyAndVisible()
-        firstViewController.setNavigationBarHidden(false, animated: true)
-        firstViewController.pushViewController(NavigationViewController(), animated: true)
+//        firstViewController.setNavigationBarHidden(false, animated: true)
+//        firstViewController.pushViewController(NavigationViewController(), animated: true)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {}
