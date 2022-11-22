9419a6df70af16b95a2336536f819576269ab652
JIwonKwon <rnjswldnjs1012@gmail.com>
2022-11-22 23:40:07 +0900

    TextField구현

--- IKU/IKU/App/SceneDelegate.swift
+++ IKU/IKU/App/SceneDelegate.swift
@@ -25,7 +25,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let storyViewController: UIViewController = UIHostingController<StoryView>(rootView: StoryView())
         
         //첫번째 화면은 일단
-        let testVC = UINavigationController(rootViewController: storyViewController)
+//        let testVC = UINavigationController(rootViewController: storyViewController)
+        let testVC = UINavigationController(rootViewController: ProfileViewController())
         let historyVC = HistoryViewController()
         let dictionaryVC = DictionaryViewController()
         
