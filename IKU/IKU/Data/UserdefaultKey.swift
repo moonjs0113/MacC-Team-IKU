ce858c1c6df73c1eb5b4e10b8c646fde8411efa5
moonjs0113 <littlemoom@naver.com>
2022-11-29 11:43:38 +0900

    Save & Edit Profile Info

---
+++ IKU/IKU/Data/UserdefaultKey.swift
@@ -0,0 +1,12 @@
+//
+//  UserdefaultKey.swift
+//  IKU
+//
+//  Created by Moon Jongseek on 2022/11/28.
+//
+
+import Foundation
+
+enum UserDefaultsKey: String {
+    case PROFILE_LOCALIDENTIFIER
+}
